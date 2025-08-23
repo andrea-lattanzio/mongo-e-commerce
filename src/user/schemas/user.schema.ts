61a1d61318bea23d165a78fc64d2968b24ef7471
Andrea Lattanzio <andrealattanziodedona@gmail.com>
2025-08-23 15:48:25 +0200

    feat(user): add user schema definition and registration in module

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,48 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+export enum UserRole {
+  ADMIN = 'admin',
+  CUSTOMER = 'customer',
+}
+
+@Schema({
+  timestamps: true, // createdAt, updatedAT
+  versionKey: false, // removes __v
+})
+export class User {
+  @Prop()
+  firstName: string;
+
+  @Prop()
+  lastName: string;
+
+  @Prop({ unique: true }) // unique email
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop({ enum: UserRole, default: UserRole.CUSTOMER }) // enum with default value
+  role: string;
+}
+
+// schema will later be injected in the user module to get the model object
+export const UserSchema = SchemaFactory.createForClass(User);
+
+// add virtual property for full name
+// virtual properties are NOT included by default
+UserSchema.virtual('fullName').get(function () {
+  return `${this.firstName} ${this.lastName}`;
+});
+
+// rimuovo password quando serializzo a json
+UserSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    delete (ret as Partial<User>).password;
+    return ret;
+  },
+});
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), // registering the new collection
+  ],
   controllers: [UserController],
   providers: [UserService],
+  exports: [MongooseModule], // only needed if user model needs to be used in other modules
 })
 export class UserModule {}
